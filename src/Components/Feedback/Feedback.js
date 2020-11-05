7289ed41f58202c4461054836393b9453228c804
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2020-11-05 22:16:58 +0200

    final

--- src/Components/Feedback/Feedback.js
+++ src/Components/Feedback/Feedback.js
@@ -33,47 +33,43 @@ class Feedback extends Component {
      render() {
 // console.log(this.countTotalFeedback);
           return (
-
-               <section className="feedback-section">
-                    <div className="container">
-                         <SectionTitle >
-                              <h2 className="title">Please leave feedback</h2>
-                         </SectionTitle>
-                         <FeedbackOptions
-                              options={["good", "neutral", "bad"]}
-                              onLeaveFeedback={this.count}
-                         />
-                         {/* <div className="div">
+            <section className="feedback-section">
+              <div className="container">
+                <SectionTitle>
+                  <h2 className="title">Please leave feedback</h2>
+                </SectionTitle>
+                <FeedbackOptions options={["good", "neutral", "bad"]} onLeaveFeedback={this.count} />
+                {/* <div className="div">
                               <button onClick={this.count}  className='btn' name='good'>good</button>
                               <button onClick={this.count}  className='btn' name='neutral'>neutral</button>
                               <button onClick={this.count}  className='btn' name='bad'>bad</button>
                          </div> */}
 
-                         {!this.countTotalFeedback()
-                         ? <Notification/>
-
-                         : <>
-                         <SectionTitle title="Statistics" >
-                              <h2 className="title">Statistics</h2>
-                         </SectionTitle>
-                         <Statistics
-                              good={this.state.good}
-                              neutral={this.state.neutral}
-                              bad={this.state.bad}
-                              total={this.countTotalFeedback}
-                              positivePercentage={this.countPositiveFeedbackPercentage}
-                              />
-                         </>
-                         }
-                         {/* <ul className="list">
+                {!this.countTotalFeedback() ? (
+                  <Notification />
+                ) : (
+                  <>
+                    <SectionTitle>
+                      <h2 className="title">Statistics</h2>
+                    </SectionTitle>
+                    <Statistics
+                      good={this.state.good}
+                      neutral={this.state.neutral}
+                      bad={this.state.bad}
+                      total={this.countTotalFeedback}
+                      positivePercentage={this.countPositiveFeedbackPercentage}
+                    />
+                  </>
+                )}
+                {/* <ul className="list">
                               <li className="item"><span className='good_counter'>Good: {this.state.good}</span></li>
                               <li className="item"><span className='neutral_counter'>Neutral: {this.state.neutral}</span></li>
                               <li className="item"><span className='bad_counter'>Bad: {this.state.bad}</span></li>
                               <li className="item"><span className='total_counter'>Total counter: {this.countTotalFeedback()}</span></li>
                               <li className="item"><span className='positive-feedback_counter'>Positive feedback: {this.countPositiveFeedbackPercentage()}%</span></li>
                          </ul> */}
-                    </div>
-               </section>
+              </div>
+            </section>
           );
      }
 }
--- src/FeedbackOptions/FeedbackOptions.js
+++ src/FeedbackOptions/FeedbackOptions.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const FeedbackOptions = ({ onLeaveFeedback, options }) => {
-     return (
-          <div className="div">
-               {options.map((option, index) =>
-               <button
-                    key={index}
-                    onClick={onLeaveFeedback}
-                    className='btn'
-                         name={option}>{option}
-               </button>
-               )}
-                    {/* <button onClick={onLeaveFeedback}  className='btn' name='neutral'>neutral</button>
+  return (
+    <div className="div">
+      {options.map((option, index) => (
+        <button key={index} onClick={onLeaveFeedback} className="btn" name={option}>
+          {option}
+        </button>
+      ))}
+      {/* <button onClick={onLeaveFeedback}  className='btn' name='neutral'>neutral</button>
                     <button onClick={onLeaveFeedback}  className='btn' name='bad'>bad</button> */}
-          </div>
-     );
+    </div>
+  );
 };
 
 export default FeedbackOptions;
 
 FeedbackOptions.propTypes = {
      onLeaveFeedback: PropTypes.func.isRequired,
-     options: PropTypes.array.isRequired,
+     options: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
