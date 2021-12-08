cab38a56703df82eefe471501784d4c419c0ebc3
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2021-12-09 00:36:44 +0200

    finish

---
+++ src/component/Main/Main.jsx
@@ -0,0 +1,14 @@
+import Feedback from '../Feedback';
+const Main = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <Feedback/>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Main;
